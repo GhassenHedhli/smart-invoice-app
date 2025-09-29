7356b0e2c84a7eb0674e5f92062ffca48b632d74
GhassenHedhli <93828016+GhassenHedhli@users.noreply.github.com>
2025-09-29 12:43:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<?php
-// index.php
-session_start();
-
-// Check if user is logged in
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit;
-}
-
-// Otherwise, go to dashboard
-header("Location: dashboard.php");
-exit;
-?>
+<?php 
+header("Location: dashboard.php"); 
+exit; ?>
