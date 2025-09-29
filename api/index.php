6e136c32a849f8cd7496f950443723c7250f4c57
GhassenHedhli <93828016+GhassenHedhli@users.noreply.github.com>
2025-09-29 15:52:03 +0000

    deploy

---
+++ api/index.php
@@ -0,0 +1,5 @@
+<?php
+// /api/index.php redirects to dashboard
+header("Location: ../dashboard.php");
+exit;
+?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//index.php
-header("Location: dashboard.php");
+// Redirect from root to /api/index.php (or dashboard.php)
+header("Location: api/index.php"); 
 exit;
 ?>
